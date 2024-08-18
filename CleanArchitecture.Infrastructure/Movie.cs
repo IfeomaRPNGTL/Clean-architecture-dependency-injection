2aab5ba5af6e89d53be64412da0f465a6ad2be77
IfeomaRPNGTL <okatamelody@gmail.com>
2024-08-17 22:20:53 -0700

    Add project files.

---
+++ CleanArchitecture.Infrastructure/Movie.cs
@@ -0,0 +1,8 @@
+﻿namespace CleanArchitecture.Infrastructure
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+    }
+}
